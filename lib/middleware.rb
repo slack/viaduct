35b4843304411593d407937fe2d2c67fabee8bbc
Jason Hansen <jason@slack.io>
2012-08-29 19:31:19 -0700

    initial

---
+++ lib/middleware.rb
@@ -0,0 +1,6 @@
+module Middlware
+end
+
+require 'middleware/env'
+require 'middleware/warden'
+require 'middleware/builder'
---
+++ lib/middleware/env.rb
@@ -0,0 +1,19 @@
+module Middleware
+  module Env
+    # A middleware which just sets up the environment with some
+    # options which are passed to it.
+    class Set
+      def initialize(app, env, options=nil)
+        @app     = app
+        @options = options || {}
+      end
+
+      def call(env)
+        # Merge the options that were given to us
+        env.merge!(@options)
+
+        @app.call(env)
+      end
+    end
+  end
+end
